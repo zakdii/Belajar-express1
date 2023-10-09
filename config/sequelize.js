52b4a023c50fcb5acd5325967f1ede80b7d7dee0
zakdii <mzakyd2@gmail.com>
2023-10-09 19:17:43 +0700

    added dialect

--- config/sequelize.js
+++ config/sequelize.js
@@ -4,17 +4,17 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const sequelize = new Sequelize(
-  process.env.MYSQL_URL
+  process.env.MYSQL_URL,
   // process.env.MYSQLDATABASE,
   // process.env.MYSQLUSER,
   // process.env.MYSQLPASSWORD,
-  // {
-  //   dialect: "mysql",
-  //   dialectModule: mysql2,
-  //   host: process.env.MYSQLHOST,
-  //   port: process.env.MYSQLPORT,
-  //   dialect: process.env.DB_DIALECT,
-  // }
+  {
+    dialect: "mysql",
+    //   dialectModule: mysql2,
+    //   host: process.env.MYSQLHOST,
+    //   port: process.env.MYSQLPORT,
+    //   dialect: process.env.DB_DIALECT,
+  }
 );
 
 module.exports = sequelize;
